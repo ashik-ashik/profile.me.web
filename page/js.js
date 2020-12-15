8e5e142a9510c2e53b45e7bac6e659902df999c5
Ashik <kmdashik635@gmail.com>
2020-12-15 18:18:23 +0600

    mostafizur s comment addd

--- page/js.js
+++ page/js.js
@@ -2,6 +2,11 @@
 
 $(document).ready(function(){
 
+  // reply 03
+    $(".reply-able-comment3").click(function(){
+        $(".comment-reply3").toggle('slow');
+      });
+
   // reply 02
     $(".reply-able-comment2").click(function(){
         $(".comment-reply2").toggle('slow');
