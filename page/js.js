d704141a83a7e31b89a76595ade46706cc249844
Ashik <kmdashik635@gmail.com>
2020-12-15 15:42:22 +0600

    ruhul asa abdur s comment add

--- page/js.js
+++ page/js.js
@@ -1,7 +1,14 @@
 
 
 $(document).ready(function(){
-    $(".reply-able-comment").click(function(){
-        $(".comment-reply").toggle('slow');
+
+  // reply 02
+    $(".reply-able-comment2").click(function(){
+        $(".comment-reply2").toggle('slow');
+      });
+
+  //  reply 01
+    $(".reply-able-comment1").click(function(){
+        $(".comment-reply1").toggle('slow');
       });
 });
