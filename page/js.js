093b27fca98e9b76c2222d476d4c06b021a00114
Ashik <kmdashik635@gmail.com>
2020-12-17 14:17:23 +0600

    add mentor s comment

--- page/js.js
+++ page/js.js
@@ -2,18 +2,23 @@
 
 $(document).ready(function(){
 
-  // reply 03
+  // reply 04
+    $(".reply-able-comment4").click(function(){
+        $(".comment-reply4").slideToggle('slow');
+      });
+	  
+   // reply 03
     $(".reply-able-comment3").click(function(){
-        $(".comment-reply3").toggle('slow');
+        $(".comment-reply3").slideToggle('slow');
       });
 
   // reply 02
     $(".reply-able-comment2").click(function(){
-        $(".comment-reply2").toggle('slow');
+        $(".comment-reply2").slideToggle('slow');
       });
 
   //  reply 01
     $(".reply-able-comment1").click(function(){
-        $(".comment-reply1").toggle('slow');
+        $(".comment-reply1").slideToggle('slow');
       });
 });
